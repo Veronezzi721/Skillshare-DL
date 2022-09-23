b4d70b92605c6aa68f8fc7ee6d14fdd11ee15d9c
Veronezzi721 <59160683+Veronezzi721@users.noreply.github.com>
2022-09-23 17:00:08 -0300

    Update dl.py

--- dl.py
+++ dl.py
@@ -3,12 +3,12 @@ from skillshare import Skillshare, splash
 from magic import cookie
 
 # or by class ID:
-# dl.download_course_by_class_id(189505397)
+# dl.download_course_by_class_id(1735737579)
 
 def main():
     dl = Skillshare(cookie)
     course_url = sys.argv[1]
-    dl.download_course_by_url(course_url)
+    dl.download_course_by_class_id(course_url)
 
 
 if __name__ == "__main__":
